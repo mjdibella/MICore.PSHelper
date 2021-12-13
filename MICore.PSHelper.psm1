68321ebed9dfdf43491f6c416f546206336d30b6
mjdibella <74016254+mjdibella@users.noreply.github.com>
2021-12-13 14:08:52 -0800

    Add files via upload

--- MICore.PSHelper.psm1
+++ MICore.PSHelper.psm1
@@ -10,18 +10,6 @@ function Read-HostSecure {
     [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
 }
 
-function New-SecureItemProperty {
-    param(
-        [Parameter(Mandatory=$true)][string]$path,
-        [Parameter(Mandatory=$true)][string]$name,
-        [Parameter(Mandatory=$true)][string]$value
-    )
-    $tempArray = $value.ToCharArray() | % {[byte] $_}
-    $encrypted = [System.Security.Cryptography.ProtectedData]::Protect($tempArray, `
-        $null, [System.Security.Cryptography.DataProtectionScope]::CurrentUser)
-    New-ItemProperty -Path $path -Name $name -Value $encrypted -Force
-}
-
 function Connect-MICore {
     param(
         [Parameter(Mandatory=$true)][string]$coreHost,
@@ -30,9 +18,12 @@ function Connect-MICore {
     $apiPass = Read-HostSecure -prompt "Password"
     $global:coreHost = $coreHost
     $global:apiCredential = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($apiUser + ":" + $apiPass))
+    $tempArray = $global:apiCredential.ToCharArray() | % {[byte] $_}
+    $encrypted = [System.Security.Cryptography.ProtectedData]::Protect($tempArray, `
+        $null, [System.Security.Cryptography.DataProtectionScope]::CurrentUser)
     New-Item -Path $global:registryURL -Force | Out-null
     New-ItemProperty -Path $global:registryURL -Name CoreHost -Value $coreHost -Force | Out-Null
-    New-SecureItemProperty -Path $global:registryURL -Name ApiCredential -Value $apiCredential | Out-Null
+    New-ItemProperty -Path $global:registryURL -Name ApiCredential -Value $encrypted | Out-Null
     Write-host "Connected to $global:coreHost`n"
 }
 
@@ -64,7 +55,7 @@ function Add-MIDeviceLabel {
     $uri = 'https://' + $global:coreHost + '/api/v2/devices/labels/' + $name + '/add?adminDeviceSpaceId=1'
     $body = $jsonBlock + '{"deviceUuids": ["' + $Uuid + '"]}'
     $uri = [uri]::EscapeUriString($uri)
-        $webresponse = invoke-webrequest -uri $uri -headers @{"Authorization" = "Basic $global:apiCredential"} -body $body -method PUT -contentType application/json
+    $webresponse = invoke-webrequest -uri $uri -headers @{"Authorization" = "Basic $global:apiCredential"} -body $body -method PUT -contentType application/json
     $response = ConvertFrom-JSON $webresponse.Content
     $response.results.records.device
 }
@@ -117,6 +108,117 @@ function Get-MIDevice {
     }
 }
 
+function New-MIDevice {
+    param(
+        [Parameter(Mandatory=$true,ParameterSetName="fromTemplate")][string]$uuid,
+        [Parameter(Mandatory=$false,ParameterSetName="fromTemplate")][switch]$noLabels,
+        [Parameter(Mandatory=$true,ParameterSetName="fromParams")][string]$phoneNumber,
+        [Parameter(Mandatory=$true,ParameterSetName="fromParams")][string]$userId,
+        [Parameter(Mandatory=$false)][string]$operator,
+        [Parameter(Mandatory=$true,ParameterSetName="fromParams")][boolean]$isEmployeeOwned,
+        [Parameter(Mandatory=$true,ParameterSetName="fromParams")][string]$platform,
+        [Parameter(Mandatory=$true,ParameterSetName="fromParams")][string]$deviceType,
+        [Parameter(Mandatory=$false)][switch]$localUser,
+        [Parameter(Mandatory=$false)][string]$userFirstName,
+        [Parameter(Mandatory=$false)][string]$userLastName,
+        [Parameter(Mandatory=$false)][string]$userEmailAddress,
+        [Parameter(Mandatory=$false)][switch]$notifyUser,
+        [Parameter(Mandatory=$false)][switch]$notifyuserbysms = $False,
+        [Parameter(Mandatory=$false)][string]$countryCode = "1"
+
+    )
+    if ($uuid) {
+        # create new device from the values of the existing device
+        $device = get-MIDevice -uuid $uuid
+        if ($device) {
+            if (-Not $platfrom) {
+                $platformName = ((Get-MIDevice -uuid ef9c1a62-446b-4fb0-a333-5cd4658c6475).details.entry | where {$_.key -eq "platform"}).Value
+                switch ($platformName) {
+                    "iPhone" {$platform = "I"}
+                    "iOS" {$platform = "I"}
+                    "macOS" {$platform = "I"}
+                    "Android" {$platform = "I"}
+                    default {
+                        $windowsType = ((Get-MIDevice).details.entry | where {$_.key -eq "wp_os_platform"}).value
+                        if ($windowsType) {
+                            $isWindowsPhone = ((Get-MIDevice).details.entry | where {$_.key -eq "wp_phone"}).value
+                            if ($isWindowsPhone = "True") {
+                                $platform = "M"
+                            } else {
+                                $platform = "E"
+                            }
+                        }
+                    }
+                }
+            }
+            if (-Not $phoneNumber) {
+                $phoneNumber = $device.currentPhoneNumber
+            }
+            if (-Not $userId) {
+                $userId = $device.principal
+            }
+            if (-Not $operator) {
+                $operator = $device.operator
+            }
+            if (-Not $isEmployeeOwned) {
+                $isEmployeeOwned = $device.employeeOwned
+            }
+            if (-Not $deviceType) {
+                if ($phoneNumber -eq "PDA") {
+                    $deviceType = "PDA"
+                } else {
+                    $deviceType = "Phone"
+                }
+            }
+        }
+    }
+    $uri = 'https://' + $global:coreHost + '/api/v1/dm/register'
+    $uri = $uri + '?phoneNumber=' + $($phoneNumber -replace "[^0-9]")
+    $uri = $uri + '&userId=' + $userId
+    if ($operator) {
+        $uri = $uri + '&operator=' + [System.Web.HttpUtility]::UrlEncode($operator)
+    }
+    $uri = $uri + '&isEmployeeOwned=' + $isEmployeeOwned
+    $uri = $uri + '&platform=' + [System.Web.HttpUtility]::UrlEncode($platform)
+    $uri = $uri + '&deviceType=' + [System.Web.HttpUtility]::UrlEncode($deviceType)
+    $uri = $uri + '&importUserFromLdap=' + (-Not $localUser)
+    if ($userFirstName) {
+        $uri = $uri + '&userFirstName=' + [System.Web.HttpUtility]::UrlEncode($userFirstName)
+    }
+    if ($userLastName) {
+        $uri = $uri + '&userLastName=' + [System.Web.HttpUtility]::UrlEncode($userLastName)
+    }
+    if ($userEmailAddress) {
+        $uri = $uri + '&userEmailAddress=' + [System.Web.HttpUtility]::UrlEncode($userEmailAddress)
+    }
+    $uri = $uri + '&notifyuser=' + $notifyuser
+    $uri = $uri + '&notifyuserbysms=' + $notifyuserbysms
+    $uri = $uri + '&countryCode=' + $countryCode
+    try {
+        $uri
+        #$webresponse = invoke-webrequest -uri $uri -headers @{"Authorization" = "Basic $global:apiCredential"} -method PUT 
+        $registerResult = (ConvertFrom-JSON $webresponse.Content).registration
+        if (($registerResult.status -eq 'SUCESS') -or ($registerResult.status -eq 'SUCCESS')) {
+            if (($uuid) -and (-Not $noLabels)) {
+                $labelResult = Copy-MIDeviceLabels -uuid $uuid -target $registerResult.deviceUuid
+                $registerResult | Add-member -NotePropertyName labelResult -NotePropertyValue $labelResult
+            }
+        }
+        $registerResult
+    } catch {
+        Set-RESTErrorResponse
+    }
+}
+
+function Set-RESTErrorResponse {
+    $global:result = $_.Exception.Response.GetResponseStream()
+    $global:reader = New-Object System.IO.StreamReader($global:result)
+    $global:responseBody = $global:reader.ReadToEnd();
+    $global:responseError = (ConvertFrom-JSON $global:responsebody).messages
+    $global:responseError
+    break
+}
+
 function Get-MICommand {
     Get-Command | where {$_.ModuleName -eq "MICore.PSHelper"}
 }
@@ -129,10 +231,12 @@ if ($global:registryKey -eq $null) {
     Write-Warning "Autoconnect failed.  API key not found in registry.  Use Connect-MICore to connect manually."
 } else {
     $global:coreHost = $global:registryKey.CoreHost
+
     $encrypted = $registryKey.ApiCredential
     $decrypted = [System.Security.Cryptography.ProtectedData]::Unprotect($encrypted, `
         $null, [System.Security.Cryptography.DataProtectionScope]::CurrentUser)
-    $decrypted | % { $global:apiCredential += [char] $_} | Out-Null
+    $decrypted | % { $apiBase64 += [char] $_} | Out-Null
+    $global:apiCredential = $apiBase64
     Write-host "Connected to $global:coreHost"
 }
 Write-host "Cmdlets added: $(Get-Command | where {$_.ModuleName -eq "MICore.PSHelper"})`n"
